8aca3c1c45bc3742c52e777d9aff10b3e1de7fd5
muhammad <muhammad.amir@kwanso.com>
2021-06-04 14:53:03 +0500

    first commit

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton';
+
+function Header(){
+    return(
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            <img
+                className="header__logo"
+                src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Logo-700x394.png"
+                alt=""
+            />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+        </div>
+    )
+}
+
+export default Header;
